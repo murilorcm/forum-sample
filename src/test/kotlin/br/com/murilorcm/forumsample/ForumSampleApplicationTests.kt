94cf8b1ed9eb62cce6f138001e7946307927a6ba
murilorcm <murilo.rcm@gmail.com>
2022-04-05 21:47:45 -0300

    feat: generated by start.spring.io

---
+++ src/test/kotlin/br/com/murilorcm/forumsample/ForumSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.murilorcm.forumsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
