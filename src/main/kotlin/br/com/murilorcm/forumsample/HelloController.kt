7eeaed9f5025f6a2ee382920275f18910c8d767f
murilorcm <murilo.rcm@gmail.com>
2022-04-05 22:15:04 -0300

    feat: Hello World!

---
+++ src/main/kotlin/br/com/murilorcm/forumsample/HelloController.kt
@@ -0,0 +1,13 @@
+package br.com.murilorcm.forumsample
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+
+    @GetMapping
+    fun hello() = "Hello World alterado!!!"
+}
